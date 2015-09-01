93779e891b18f7721d63cb4aac646a660e30b229
ahal64 <alan_hallak@optusnet.com.au>
2015-09-02 01:06:51 +1000

    more functionality added
    basic graphs can now be generated

--- server.R
+++ server.R
@@ -1,6 +1,67 @@
+library(shiny)
+library(scales)
+library(ggplot2)
+
 shinyServer(
   function(input, output) {
   
-    marketingData <- read.csv("data/Assignment01.csv")
+    #marketingData <- NULL #read.csv("data/Assignment01.csv")
+    if(is.null(marketingData) || is.na(marketingData))
+    {
+      rawDataLink <- "https://raw.githubusercontent.com/ahal64/FIT3152/master/www/data/Assignment01.csv"
+      marketingData <- read.csv(rawDataLink)
+      if(is.null(marketingData) || is.na(marketingData))
+      {
+        marketingData <- read.csv(url(rawDataLink))
+      }
+    }
+    
+    output$graph<- renderPlot({
+      dataCol<-input$graphDisp
+      graphGeom <- input$graphType
+      pos<-input$pos #dodge or stacked
+      
+      if(graphGeom == "Bar")
+      {
+        graph <- geom_bar(stat="identity")
+      }
+      else if(graphGeom == "Line")
+      {
+        graph <- geom_line(stat="identity")
+      }
+      else if(graphGeom == "Point")
+      {
+        graph <- geom_point(stat="identity")
+      }
+      else
+      {
+        graph <- geom_density(stat="identity")
+      }
+      
+
+      #PromotionType & Sales Unit
+      if(dataCol == 1)
+      {
+        qplot(marketingData$promotion_type,
+              marketingData$sales_unit,
+              fill=marketingData$category_name,
+              position=pos,
+              data=marketingData)+scale_y_continuous(labels = comma)+graph
+      }
+      #PromotionType & Sales Velocity
+      else if(dataCol == 2)
+      {
+        qplot(marketingData$promotion_type,
+              marketingData$sales_velocity,
+              fill=marketingData$category_name,
+              position=pos,
+              data=marketingData)+scale_y_continuous(labels = comma)+graph
+      }
+      else
+      {
+        
+      }
+      
+    })
   
 })
--- ui.R
+++ ui.R
@@ -1,3 +1,7 @@
+library(shiny)
+library(scales)
+library(ggplot2)
+
 shinyUI(fluidPage(
   
   titlePanel(
@@ -6,9 +10,30 @@ shinyUI(fluidPage(
   
   sidebarLayout(
     sidebarPanel(
-      img(src="images/marketing_sales.png",height = 120, width = 120,align = "center")
+      
+      selectInput("graphDisp", 
+                  label = ("Select Information to Display"), 
+                  choices = list("PromotionType & Sales Unit" = 1,
+                                 "PromotionType & Sales Velocity" = 2), 
+                  selected = 1),
+      
+      selectInput("graphType", 
+                  label = ("Select Graph Type"), 
+                  choices = c("Bar","Line","Point","Density"), 
+                  selected = "Bar"),
+      radioButtons("pos", label = "Position",
+                   choices = c("stack","dodge"),selected = "dodge"),
+      selectInput("Facet",
+                  label=("Facet Graph"),
+                  choices = c("None",
+                              "Sales Departments",
+                              "Sales Month"),
+                  selected = "None")
+      
+      
+      #,img(src="images/marketing_sales.png",height = 120, width = 120,align = "center")
       ),
-    mainPanel()
+    mainPanel(plotOutput("graph"))
   )
   
 ))
